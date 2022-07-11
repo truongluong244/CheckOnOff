02c018dee8544873c70e34e26afa430093a445ab
Luo <os@LuoMacBook-Pro.local>
2022-07-11 10:16:00 +0700

    on-off

---
+++ app.js
@@ -0,0 +1,28 @@
+
+function hasNetwork(online) {
+    const element = document.querySelector(".status");
+    // Update the DOM to reflect the current status
+    if (online) {
+      element.classList.remove("offline");
+      element.classList.add("online");
+      element.innerText = "Online";
+    } else {
+      element.classList.remove("online");
+      element.classList.add("offline");
+      element.innerText = "Offline";
+    }
+  }
+ 
+  window.addEventListener("load", () => {
+    hasNetwork(navigator.onLine);
+  window.addEventListener("online", () => {
+      // Set hasNetwork to online when they change to online.
+      hasNetwork(true);
+    });
+  
+  window.addEventListener("offline", () => {
+      // Set hasNetwork to offline when they change to offline.
+      hasNetwork(false);
+    });
+  });
+  console.log(setTimeout(document.visibilityState, 2000))
